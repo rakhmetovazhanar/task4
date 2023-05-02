46922b6cd3fb3ac22c17a056e01b8e0859e01695
zhanarrakhmetova <82767082+rakhmetovazhanar@users.noreply.github.com>
2023-05-02 14:29:12 +0600

    Update first_script.py

--- first_script.py
+++ first_script.py
@@ -9,3 +9,9 @@ def test_my_fast():
 def test_my():
     number = 100
     assert 99+1 == number
+    
+ @pytest.mark.slow
+def test_my_zh():
+    number2 = 1000
+    assert 999+1 == number2   
+    
